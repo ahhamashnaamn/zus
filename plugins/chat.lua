4c11e06bd5d5ced90bce4ecd79f56eed3ade7a70
ahhamashnaamn <ccaahh430@gmail.com>
2016-01-30 18:14:03 +0000

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,9 +1,9 @@
 local function run(msg)
-if msg.text == "hi" then
-	return "Hello bb"
+if msg.text == "سلام" then
+	return "؟سلام عشقم خوبی"
 end
-if msg.text == "Hi" then
-	return "Hello honey"
+if msg.text == "کیر" then
+	return "تو کسس ننت"
 end
 if msg.text == "Hello" then
 	return "Hi bb"
@@ -11,25 +11,34 @@ end
 if msg.text == "hello" then
 	return "Hi honey"
 end
+if msg.text == "کسس ننت" then
+	return "کیر آقام دست ننت"
+end
 if msg.text == "Salam" then
 	return "Salam aleykom"
 end
 if msg.text == "salam" then
 	return "va aleykol asalam"
 end
-if msg.text == "zac" then
-	return "Barash bezan sak"
+if msg.text == "ali" then
+	return "با باباییم چیکار داری؟"
 end
-if msg.text == "Zac" then
-	return "Barash bezan sak"
+if msg.text == "ناموسا" then
+	return "ننت و آقام چالوسن"
+end
+if msg.text == "علی" then
+	return "علی بیا این پشمک کارت داره!
 end
 if msg.text == "ZAC" then
 	return "Barash bezan sak"
 end
-if msg.text == "Umbrella" then
+if msg.text == "pob" then
 	return "Yes?"
 end
-if msg.text == "umbrella" then
+if msg.text == "خوبی" then
+	return "فدات تو خوبی"
+end	
+if msg.text == "Pob" then
 	return "What?"
 end
 if msg.text == "bot" then
@@ -61,6 +70,15 @@ return {
 		"^[Uu]mbrella$",
 		"^[Bb]ye$",
 		"^?$",
+		"^سلام$",
+		"^خوبی$",
+		"^علی$",
+		"^کس ننت$",
+		"^ناموسا$",
+		"^ali$",
+		"^Ali$",
+		"^pob$",
+		"^کیر$",
 		"^[Ss]alam$",
 		}, 
 	run = run,
